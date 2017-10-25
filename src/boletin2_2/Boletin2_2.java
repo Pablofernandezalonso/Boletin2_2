2819d9287b768195bf422cad4b1d6858b730d235
pfernandezalonso <pfernandezalonso@victoria29.danielcastelao.org>
2017-10-25 09:40:04 +0200

    Boletin2_2 Terminado

---
+++ src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,18 @@
+
+package boletin2_2;
+
+
+public class Boletin2_2 {
+
+    
+    public static void main(String[] args) {
+        
+        float lado=3F,area;
+        //area=lado*lado;
+        //Tamen
+        area=(float)Math.pow(lado,2);
+        System.out.println("Area = "+area);
+        
+    }
+    
+}
